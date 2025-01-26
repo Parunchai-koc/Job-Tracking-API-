c6af8aba6c64475e891a0c43d8815b05ddbc1634
Parunchai-koch <parunchai.koch@gmail.com>
2025-01-26 17:48:38 +0700

    final version

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +22,16 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);//Ensures that the rate limiter correctly identifies the client’s IP address.
+app.use(rateLimiter({//Prevents brute-force attacks and DDoS attacks by limiting excessive requests & from being overwhelmed by too many requests.
+  windowMs : 15 * 60 * 1000,// 15 minutes
+  max : 100// Limit each IP to 100 requests per windowMs
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet());//secure your Express app by setting various HTTP headers.
+app.use(cors());//middleware that allows or restricts requests from different origins (domains).
+app.use(xss());//It cleanses data in req.body, req.query, and req.params by removing or encoding malicious scripts.
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
